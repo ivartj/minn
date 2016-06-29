7ec0e0c74909393a95819c1ab23c5a8ddcf2df3f
Ivar Trygve Jarlsby <ivartj@ivartj.org>
2016-06-29 11:39:15 +0200

    Made create command based on migration path finding.

--- cmd-create.go
+++ cmd-create.go
@@ -45,38 +45,18 @@ func cmdCreate(cmd *cmdContext) {
 
 	cmdCreateArgs(cmd)
 
-	_, err := cmd.Exec(`
-
-		create table schema_changes (
-			schema_version text not null,
-			program_version text not null,
-			change_time text not null
-		);
-
-		create table cards (
-			card_id integer not null primary key,
-			entry_time text not null,
-
-			front text not null unique,
-			back text not null,
-
-			efactor float not null,
-			interval integer not null,
-
-			-- 0 new
-			-- 1 relearn
-			-- 2 review
-			state integer not null,
-
-			schedule_time text not null
-		);
-
-		insert into schema_changes values (?, ?, ?);
-
-	`, mainSchemaVersion, mainProgramVersion, cmd.Now().Format(utilTimeFormat))
-
+	path, err := dbGetMigrationPath("", mainSchemaVersion)
 	if err != nil {
-		cmd.Fatalf("Error on creating database: %s.\n", err.Error())
+		cmd.Fatalf("Did not find migration path to current schema version: %s.\n", err.Error())
+	}
+
+	for _, m := range path {
+
+		_, err = cmd.Exec(m.code)
+		if err != nil {
+			cmd.Fatalf("Failed to apply migration %s -> %s.\n", m.from, m.to)
+		}
+
 	}
 
 	cmd.Commit()
